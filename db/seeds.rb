45c91d13b69110cae29edd6b93477c4da4ad8c15
hayashi-3 <hayashi.gml@gmail.com>
2020-12-06 16:15:50 +0900

    [add]seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Branch.create!(
+  name: "あんまん支店",
+  area: "東京都",
+  evaluation: "3"
+)
+
+Event.create!(
+  branch_id: "1",
+  date: "2020-12-10",
+  station: "笹塚駅",
+  postal_code: "1001701",
+  prefecture_code: "13",
+  city: "青ヶ島村",
+  street: "青ヶ島村一円",
+  building: "我が家"
+)
